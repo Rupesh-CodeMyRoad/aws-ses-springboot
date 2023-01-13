afec7bddb2edb7c1a57251c7988ac0e5ed0bc8cd
Rupesh <rupeshgaudel3@gmail.com>
2023-01-13 10:06:29 +0545

    code base

---
+++ src/test/java/com/xgileit/aws/ses/SesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgileit.aws.ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
