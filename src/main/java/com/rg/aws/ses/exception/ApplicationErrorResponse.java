03fb8ad5bb337bcfd9f1a0ec8e3fc7ec734080a2
Rupesh <rupeshgaudel3@gmail.com>
2023-01-19 11:14:24 +0545

    Added JavaDoc

--- src/main/java/com/rg/aws/ses/exception/ApplicationErrorResponse.java
+++ src/main/java/com/rg/aws/ses/exception/ApplicationErrorResponse.java
@@ -1,20 +1,21 @@
 package com.rg.aws.ses.exception;
 
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Represents a JSON response for all the caught exceptions
+ */
+@Generated
 @Builder
 @Data
 public class ApplicationErrorResponse {
-	
-	
 
-	private String errorMessage;
-
+    private String errorMessage;
     private HttpStatus errorCode;
-
     private String customMessage;
 
 }
