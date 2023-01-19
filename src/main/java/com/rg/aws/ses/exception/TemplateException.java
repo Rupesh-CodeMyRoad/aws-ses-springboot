7eeead3af01f18d9a1d074f5390ce40ce442f15c
Rupesh <rupeshgaudel3@gmail.com>
2023-01-19 11:19:13 +0545

    refactored TemplateException

--- src/main/java/com/rg/aws/ses/exception/TemplateException.java
+++ src/main/java/com/rg/aws/ses/exception/TemplateException.java
@@ -3,29 +3,21 @@ package com.rg.aws.ses.exception;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+/**
+ * This is the Template Not Found exception thrown when there is no template found on aws cloud
+ * request being sent
+ */
 @ToString
 @EqualsAndHashCode(callSuper = true)
 public class TemplateException extends RuntimeException{
 
-	public TemplateException() {
-		super();
-	}
-
-	public TemplateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 	public TemplateException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
 	public TemplateException(String message) {
 		super(message);
 	}
 
-	public TemplateException(Throwable cause) {
-		super(cause);
-	}
 	
 	
 
