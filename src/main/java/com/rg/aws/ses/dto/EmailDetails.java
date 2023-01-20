f305e7cf9aa83f9c653a8e7215b2318ec69bf672
Tebogo Assagaai <tebogo.assagaai@xgileit.com>
2023-01-20 09:42:26 +0200

    test cases

--- src/main/java/com/rg/aws/ses/dto/EmailDetails.java
+++ src/main/java/com/rg/aws/ses/dto/EmailDetails.java
@@ -2,10 +2,12 @@ package com.rg.aws.ses.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class EmailDetails {
